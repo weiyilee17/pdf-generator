c2e14cf1af823bb17ddde3f05600a4838d2372e4
Weiyi Lee <weiyilee17@gmail.com>
2023-02-04 15:11:30 +0800

    Added footers

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from fpdf import FPDF
 from pandas import read_csv
 
 pythonNotesPDF = FPDF(orientation='P', unit='mm', format='A4')
+pythonNotesPDF.set_auto_page_break(auto=False, margin=0)
 
 topics_data_frame = read_csv(filepath_or_buffer='topics.csv')
 
-pythonNotesPDF.set_font('Times', style='B', size=24)
-pythonNotesPDF.set_text_color(100, 100, 100)
 
 for index, row in topics_data_frame.iterrows():
 
     pythonNotesPDF.add_page()
 
+    # Set the header
+    pythonNotesPDF.set_font('Times', style='B', size=24)
+    pythonNotesPDF.set_text_color(100, 100, 100)
     """
     w=0 : width would extend to the end of page
-    ln=1 : break line before next cell
+    ln=1 : one break line, each 1mm
     """
     pythonNotesPDF.cell(w=0, h=12, txt=row['Topic'], ln=1)
 
@@ -28,15 +30,27 @@ for index, row in topics_data_frame.iterrows():
         0,1
         0,2
         
-        In A4 format, full width is 210
+        In A4 format, full width is 210, full height is 298
         
         """
     pythonNotesPDF.line(x1=10, y1=21, x2=200, y2=21)
 
-    blank_pages = int(row['Pages']) - 1
+    # Set the footer
+    pythonNotesPDF.ln(265)
+    pythonNotesPDF.set_font('Times', style='I', size=8)
+    pythonNotesPDF.set_text_color(180, 180, 180)
+    pythonNotesPDF.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
+
+    blank_pages = row['Pages'] - 1
 
     for i in range(blank_pages):
         pythonNotesPDF.add_page()
+
+        # Set the footer
+        pythonNotesPDF.ln(277)
+        pythonNotesPDF.set_font('Times', style='I', size=8)
+        pythonNotesPDF.set_text_color(180, 180, 180)
+        pythonNotesPDF.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
 
 
 pythonNotesPDF.output('notes.pdf')
